543ea96e2d547af070f8e22faba2587444e1e3e9
sh3llf1sh <lukerixson@gmail.com>
2018-02-10 08:58:40 -0500

    added mass reverse dns enum functionality

--- dnsenum.sh
+++ dnsenum.sh
@@ -3,6 +3,12 @@
 if [ "$1" == "" ]; then
 	echo "please supply a domain to enummerate e.g ./dnsenum google.co.uk"
         exit
+elif [ "$1" == "-r" ]; then
+     
+     echo "Please provide the first three octets of the ip \n" 
+     read cidr
+     for ip in $(seq 1 254); do host 10.11.1.$ip |grep -v "not found"; done
+       exit 0
 else
 	host=$1
 fi
